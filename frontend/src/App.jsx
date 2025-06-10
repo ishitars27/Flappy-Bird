1ebf38651cfa5910ccaa64b65f7743a11dd778f7
Vinayak Singh <ishita.rs27@gmail.com>
2025-06-10 12:36:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,31 +8,34 @@ import Login from "./components/pages/Login";
 import Dashboard from "./components/pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute"; // Add this import
 import LeaderboardPage from "./components/pages/LeaderboardPage";
-import Notfound from "./components/pages/NotFound";
+import Notfound from "./components/pages/Notfound";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    // Remove <BrowserRouter> here as it's already in main.jsx
-    <Routes>
-      {/* playbg( */}
-      {/* Public routes */}
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgetpassword" element={<ForgetPassword />} />
-      <Route path="/otp" element={<OtpVerify />} />
-      <Route path="/update/password" element={<UpdatePassword />} />
-      <Route path="/leaderboard" element={<LeaderboardPage />} />
-      {/* Protected route for Dashboard */}
-
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <>
+      <Routes>
+        {/* playbg( */}
+        {/* Public routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+        <Route path="/otp" element={<OtpVerify />} />
+        <Route path="/update/password" element={<UpdatePassword />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="*" element={<Notfound />} />
+        {/* Protected route for Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 

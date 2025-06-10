7eec94ab5e9e330004a2d7dd0d02552a2587f1e9
Vinayak Singh <ishita.rs27@gmail.com>
2025-06-10 20:15:25 +0530

    Remove all project files

--- backend/middlewear/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || "Server Error";
-
-  res.status(statusCode).json({ message });
-};
-
-module.exports = errorHandler;
